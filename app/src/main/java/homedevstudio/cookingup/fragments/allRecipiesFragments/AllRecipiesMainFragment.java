917e0af206b95e47d4351f4ef68f35853962f2a1
ilya <ilya@flashdev.od.ua>
2017-08-24 18:24:23 +0300

    added project structure + recipes layout

---
+++ app/src/main/java/homedevstudio/cookingup/fragments/allRecipiesFragments/AllRecipiesMainFragment.java
@@ -0,0 +1,12 @@
+package homedevstudio.cookingup.fragments.allRecipiesFragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by xdewnik on 24.08.2017.
+ */
+
+public class AllRecipiesMainFragment extends Fragment {
+
+
+}
